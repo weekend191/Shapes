46aabf0c01ca4fdfed55146007c072d1f8b56362
weekend191 <kazimierekmaciej@gmail.com>
2017-10-02 15:08:30 +0100

    Added App class
    Created the default App class and Run method

---
+++ App.cs
@@ -0,0 +1,18 @@
+using System;
+
+namespace Shapes
+{
+    class App
+    {
+        public App()
+        {
+            Console.WriteLine("Constructing the App");
+
+        }
+
+        public void Run()
+        {
+            Console.WriteLine("Hello Maciej");
+        }
+    }   
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Shapes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            App myApp = new App();
+            myApp.Run();
+        }
+    }
+}
